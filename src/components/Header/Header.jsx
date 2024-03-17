66ca9747c948c31eada704d9c8fc54d13d24cbd6
msahrana <msahrana@gmail.com>
2024-03-17 09:51:22 +0600

    all done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
 const Header = () => {
   return (
     <div className="mt-6">
-      <h1 className="text-4xl font-bold text-center">React FakeData</h1>
+      <h1 className="text-4xl font-bold text-center text-green-500 mb-6">
+        React FakeData
+      </h1>
+      <div className="divider"></div>
     </div>
   );
 };
